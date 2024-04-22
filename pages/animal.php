fbb6e39f2440fa30ca5900d92686cd2b4be96d11
Aveson <avesoncarvalho25@gmail.com>
2024-04-22 09:38:41 +0100

    starting search and search validation with api

--- pages/animal.php
+++ pages/animal.php
@@ -8,6 +8,49 @@ $webpage->setScript("../scripts/animal.js");
 
 require_once("../includes/header.inc.php"); ?>
 
-<main></main>
+<main>
+    <div class="container mt-4">
+        <!-- Search -->
+        <div class="input-group mb-3">
+            <!-- Filtering -->
+            <select class="form-control search-select shadow-none" required>
+                <option selected disabled>Filter</option>
+                <option>Land</option>
+                <option>Air</option>
+            </select>
+            <!-- Search Bar -->
+            <input type="text" placeholder="Search..." class="form-control" id="search" name="search">
+            <!-- Search Button -->
+            <button class="btn btn-light border">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search">
+                    <circle cx="11" cy="11" r="8"></circle>
+                    <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                </svg>
+            </button>
+        </div>
+        <!-- Animal -->
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                    </svg>
+                    <div class="card-body">
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            </div>
+                            <small class="text-body-secondary">9 mins</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
 
 <?php require_once("../includes/footer.inc.php"); ?>
